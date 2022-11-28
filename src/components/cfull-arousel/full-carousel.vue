bc79974202622b3e6aad579597bddefc661ad17e
minnan0328 <minnan0328@gmail.com>
2022-11-28 22:03:38 +0800

    【新增】full-carousel component

---
+++ src/components/cfull-arousel/full-carousel.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="full-carousel">
+        
+    </div>
+</template>
+
+<style lang="scss" scoped src="./full-carousel.scss"></style>
+
+<script>
+import { ref, reactive, computed, watch, onMounted } from 'vue';
+
+export default {
+    props: {
+        
+    }
+}
+</script>
